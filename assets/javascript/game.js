0007eb82137b2ecc5bd52a3242f9c7b0047de901
Julie <154025@dadeschools.net>
2018-12-15 18:45:01 -0500

    js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,57 @@
-window.onload = function () {
-    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var topics = ["Who lives in a pineapple under the sea?", "What shows character had a football head?", "What show has pickle has their last name?", "What show had two conjoined brothers?"]
-    var getHint = []
+var shows = ["Rugrats", "Doug", "CatDog", "Arthur", "Spongebob"];
+var lettersGuessed = [];
+var choosenShow = [];
+var wrongWord = [];
+var underscore = []; 
+var guessLeft = 10;
+
+var docUnderscore = document.getElementById('underscore');
+var docRightGuess = document.getElementById('rightGuess');
+ 
+
+var categories = document.getElementById("cartoons"); 
+var lettersGuessed = document.getElementById("lettersGuessed");
+
+var randNum = Math.floor(Math.random() + shows.length);
+var choosenShow = shows[randNum];
+var underscore = [];
+ 
+
+var generateUnderscore = () => {
+    for(var i = 0; i < choosenShow,length; i++){
+        underscore.push('_');
+        docUnderscore[0].innerHtml = underscore.join('');  
+    }
+    return underscore;
 }
+
+
+document.addEventListener('keypress', (event) =>{
+    var keyword = String.fromCharCode(event.keycode);
+    if(choosenShow.indexOf(keyword) ) {
+        rightWord.push(keyword);
+        docUnderscore[0].innerHTML = underscore.join('');
+        underscore[choosenShow.indexOf(keyword)] = keyword;
+        docRightGuess[0].innerHTML = rightWord;
+        if(underscore.join('') == choosenShow){
+            alert('YOU WIN!')
+        }
+    }  
+    else {
+        wrongWord.push(keyword);
+    }
+}); 
+
+generateUnderscore(); 
+// guessText.textContent  = "Guesses Left:" + guessLeft;
+
+shows.textContent = "Guess the Cartoon" ;
+//guess.textContent = "";
+//letterGuessText.textContent = "Letters guessed:" +  letterGuess;
+
+//var startButton = document.getElementById("start")[0];
+
+//shows
+
+
+
