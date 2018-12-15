52cd86fb3211b2aa6d91b74ad853d1abeff0726a
Julie <154025@dadeschools.net>
2018-12-15 11:55:23 -0500

    background

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var topics = ["Who lives in a pineapple under the sea?", "What shows character had a football head?", "What show has pickle has their last name?", "What show had two conjoined brothers?"]
+    var getHint = []
+}
